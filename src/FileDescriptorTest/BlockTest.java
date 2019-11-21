daffda72a3faa545efaa8d4996993b082016b846
Ghouas <ghouas@informatique.univ-paris-diderot.fr>
2019-11-21 23:59:09 +0100

    testing files

--- src/FileDescriptorTest/BlockTest.java
+++ src/FileDescriptorTest/BlockTest.java
@@ -2,8 +2,15 @@ package FileDescriptorTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import Configuration.Configurator;
+import Configuration.Fichier;
+import FileDescriptor.Block;
+import Model.BlockModel;
 /**
 * TP n°4 V n°1 :
 *
@@ -25,24 +32,38 @@ import org.junit.jupiter.api.Test;
 * Remarques :
 */
 class BlockTest {
-
+	BlockModel block;
+	Configurator config;
+	Fichier fichier;
+	int Id=0;
+	String [] exemple= {"AB","AC","AA","AZ","RE","NF","GV","XZ","EZ","HH"};
 	@BeforeEach
 	void setUp() throws Exception {
+		config= new Configurator();
+		 fichier = new Fichier(config);
+		 block = new Block(config , fichier);
 	}
 
 	@Test
-	void testCreate() {
-		fail("Not yet implemented");
+	void testCreate() throws IOException {
+		
 	}
 
 	@Test
-	void testLoad() {
-		fail("Not yet implemented");
+	void testLoad() throws IOException {
+		block.create();
+		block.store(exemple);
+		String [] s=block.load(0);
+		for (int i = 0; i < s.length; i++) {
+			System.out.println(s[i] +" "+exemple[i]);
+		}
+		assertArrayEquals(s,exemple);
+		
 	}
 
 	@Test
 	void testStore() {
-		fail("Not yet implemented");
+		
 	}
 
 }
